b665192863f7b957a7ea6749c3bc55fe09aa438c
solomon-hackett <solomonhackett2008@gmail.com>
2026-05-08 20:16:48 +0100

    first commit. begun work on the first page and layout

---
+++ app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Welcome to SoloStream, my mini streaming platform porject.",
+};
+
+export default function Page() {
+  return <main></main>;
+}
